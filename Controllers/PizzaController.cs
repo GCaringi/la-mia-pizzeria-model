e22da2debd60e5aee046bece83c3b9c2a45caca5
GCaringi <giannicaringi94@gmail.com>
2022-10-04 15:41:54 +0200

    Finish exercise

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -1,6 +1,49 @@
-﻿namespace la_mia_pizzeria_model.Controllers;
+﻿using la_mia_pizzeria_model.Models;
+using Microsoft.AspNetCore.Mvc;
 
-public class PizzaController
+namespace la_mia_pizzeria_model.Controllers;
+
+public class PizzaController : Controller
 {
-    
+    private readonly List<Pizza> _pizzas = new()
+    {
+        new Pizza
+        {
+            Id = 1, Name = "Margherita", Description = "Pomodoro Mozzarella", Image = "margherita.png", Price = 4.00m
+        },
+        new Pizza
+        {
+            Id = 2, Name = "Diavola", Description = "Margherita + Salame", Image = "diavola.png", Price = 7.00m
+        },
+        new Pizza
+        {
+            Id = 3, Name = "Provola", Description = "Bianca con Provola", Image = "provola.png", Price = 6.00m
+        },
+        new Pizza
+        {
+            Id = 4, Name = "Paperino", Description = "Bianca con Patatine", Image = "paperino.png", Price = 9.00m
+        },
+        new Pizza
+        {
+            Id = 5, Name = "Romanina", Description = "Rossa aglio origano", Image = "romanina.png", Price = 6.00m
+        }
+
+    };
+
+    public IActionResult Index()
+    {
+        return View(_pizzas);
+    }
+
+    public IActionResult ShowById(int id)
+    {
+        Pizza? pizza = _pizzas.Find(x => x.Id == id);
+        
+        if (pizza == null)
+        {
+            return View("Error");
+        }
+        
+        return View(pizza);
+    }
 }
---
+++ Models/Pizza.cs
@@ -0,0 +1,10 @@
+﻿namespace la_mia_pizzeria_model.Models;
+
+public class Pizza
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Image { get; set; }
+    public decimal Price { get; set; }
+}
